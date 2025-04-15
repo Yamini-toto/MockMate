4a010dc5b372c7837b48be4b1dd5dd84d791bf77
Prachi Deopa <deopaprachi60@gmail.com>
2025-04-15 22:27:49 +0530

    feat: add mock interview table and schema

---
+++ drizzle.config.js
@@ -0,0 +1,7 @@
+export default {
+    schema: "./utils/schema.js",
+    dialect: 'postgresql',
+    dbCredentials: {
+        url: 'postgresql://neondb_owner:npg_c5RsOalFj2IL@ep-solitary-sun-a4499ed9-pooler.us-east-1.aws.neon.tech/ai-interview-mocker?sslmode=require'
+    }
+}
